df91faae05be108f22e53b3f4a8971a1ff91fbd1
Ben Drucker <ben.drucker@icloud.com>
2023-10-17 12:36:51 -0500

    install apparently working (no changes to `map_peptide_position`.)

--- R/plot_sel_feat_with_p_vals.R
+++ R/plot_sel_feat_with_p_vals.R
@@ -7,27 +7,70 @@ adj.P.Val <- NULL # (For lintr)
 #'     values as the frequency at which said features were selected
 #' @param msnset A \code{MSnSet} object that represented the input to an `rf_modeling` \
 #'     call (with Boruta turned on). This is required so P-values can be computed
+#' @param response_colname The column name (in \code{pData(msnset)}) of the machine \
+#'     learning response variable
+#' @param feat_name_colname If provided, a column in \code{fData(msnset)} by which \
+#'     to name the features by. If \code{NULL}, then \
+#'     \code{featureNames(msnset) will be used.}
+#' @param highlight_feats If provided, put an asterisk ❉ next to the label of these \
+#'     features. Note: this uses \code{feat_name_colnames} if passed, and not \
+#'     \code{msnset}'s native features.
+#' @param highlight_reason Short string describing why certain features would be \
+#'     highlighted. Only applicable if \code{highlight_feats} is not \code{NULL}.
 #' @param alpha The alpha-level that defines statistical significance (for purposes \
 #'     of the color scale)
-plot_sel_feat_with_p_vals <- function(top_selected, msnset, alpha = 0.05) {
+plot_sel_feat_with_p_vals <- function(
+    top_selected,
+    msnset,
+    response_colname,
+    feat_name_colname = NULL,
+    highlight_feats = NULL,
+    highlight_reason = NULL,
+    alpha = 0.05) {
     lab <- as.vector(names(top_selected))
     cnt <- as.vector(unlist(unname(top_selected)))
+    if (!is.null(feat_name_colname)) {
+        display_lab <- Biobase::fData(msnset)[lab, ] %>% dplyr::pull(feat_name_colname)
+    } else {
+        display_lab <- lab
+    }
+    if (!is.null(highlight_feats)) {
+        display_lab <- unlist(unname(
+            lapply(
+                display_lab,
+                function(dl) ifelse(dl %in% highlight_feats, paste("❉ ", dl, sep = ""), dl)
+            )
+        ))
+    }
+
     df_from_counts <- data.frame(
         lab = lab,
+        display_lab = display_lab,
         cnt = cnt
     )
 
-    limma <- MSnSet.utils:::.get_limma(top_selected, msnset, alpha)
+    limma <- MSnSet.utils:::.get_limma(top_selected, msnset, alpha, response_colname)
     df <- merge(df_from_counts, limma, by.x = "lab", by.y = 0)
     labels_breaks <- append(
         c(1, alpha),
         unlist(lapply(seq(-2, -10, -2), function(x) 10^x))
     )
     hard_cut <- scales::rescale(x = log10(labels_breaks), to = c(0, 1))[2]
+    if (!is.null(highlight_reason) && !is.null(highlight_feats)) {
+        title_if_else <- labs(
+            title = "Frequency of\nBoruta-Selected Features",
+            subtitle = glue::glue("❉ = {highlight_reason}")
+        )
+    } else {
+        title_if_else <- labs(
+            title = "Frequency of\nBoruta-Selected Features"
+        )
+    }
+
     p <- ggplot(data = df) +
         geom_col(
             mapping = aes(
-                x = forcats::fct_reorder(lab, cnt, .desc = TRUE),
+                x = forcats::fct_reorder(display_lab, cnt, .desc = TRUE),
                 y = cnt,
                 fill = adj.P.Val
             ),
@@ -35,9 +78,7 @@ plot_sel_feat_with_p_vals <- function(top_selected, msnset, alpha = 0.05) {
         ) +
         xlab("Feature Name") +
         ylab("Counts") +
-        labs(
-            title = "Frequency of\nBoruta-Selected Features"
-        ) +
+        title_if_else +
         ggplot2::theme(
             axis.text.x = element_text(angle = 50, hjust = 1, vjust = 1),
             aspect.ratio = 1 / 2,
@@ -67,11 +108,11 @@ plot_sel_feat_with_p_vals <- function(top_selected, msnset, alpha = 0.05) {
     return(p)
 }
 
-.get_limma <- function(top_selected, msnset, alpha) {
+.get_limma <- function(top_selected, msnset, alpha, response) {
     l <- MSnSet.utils::limma_a_b(
         msnset,
-        model.str = "~ Case.Status",
-        coef.str = "Case.Status"
+        model.str = glue::glue("~ {response}"),
+        coef.str = response
     )
     ll <- l %>%
         merge(
